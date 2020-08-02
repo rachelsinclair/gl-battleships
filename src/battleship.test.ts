14c18a6ec361d54be482766642d119b030ed9d85
Rachel Sinclair <12307504+rachelsinclair@users.noreply.github.com>
2020-08-02 18:27:03 +0100

    remove TypeScript & related dependencies

--- src/battleship.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe } from "mocha";
-import { expect } from "chai";
-import { battleship } from "./battleship";
-
-describe("Battleship game", function() {
-    it("should run", function() {
-        expect(battleship()).to.be.undefined;
-    })
-})
